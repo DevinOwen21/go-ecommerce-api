8e0b7897479b7b37a4b9c67bacbe5a1f059f7bf2
Devin Owen <devin.owen01@gmail.com>
2026-07-10 00:59:48 +0700

    Prepare user registration

---
+++ repository/user_repository.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"database/sql"
+	"go-ecommerce-api/model"
+)
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) CreateUser(user model.User) error {
+	_, err := r.db.Exec("INSERT INTO users(name, email, password_hash) VALUES (?, ?, ?)", user.Name, user.Email, user.PasswordHash)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *UserRepository) GetUserByEmail(email string) (*model.User, error) {
+	var user model.User
+	row := r.db.QueryRow("SELECT name, email, password_hash from users where email = ?", email)
+	err := row.Scan(
+		&user.Name,
+		&user.Email,
+		&user.PasswordHash,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
